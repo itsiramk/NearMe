e0c30a5c040b19bccfcbbc886a6ccfb6e4744087
itsiramkhan <khan.iram02@gmail.com>
2022-04-30 18:55:27 +0530

    Added markers to view nearby locations on map

--- app/src/main/java/com/adyen/android/assignment/ui/MapsFragment.kt
+++ app/src/main/java/com/adyen/android/assignment/ui/MapsFragment.kt
@@ -10,18 +10,17 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.adyen.android.assignment.R
-import com.adyen.android.assignment.adapter.PlacesAdapter
 import com.adyen.android.assignment.api.VenueRecommendationsQueryBuilder
 import com.adyen.android.assignment.api.model.GeoCode
 import com.adyen.android.assignment.utils.ActivityUtils
 import com.adyen.android.assignment.utils.PermissionUtils
 import com.adyen.android.assignment.viewmodel.PlacesViewModel
+import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -33,7 +32,6 @@ import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_maps.*
-import javax.inject.Inject
 
 
 @AndroidEntryPoint
@@ -50,51 +48,25 @@ class MapsFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClickListe
     private lateinit var currentLocation: Location
     private var nearbylocationList: MutableList<GeoCode> = ArrayList()
     private var isPermissionGranted: Boolean = false
-    private lateinit var mView: View
-
-    @Inject
-    lateinit var adapter: PlacesAdapter
-
+    private lateinit var fusedLocationProviderClient : FusedLocationProviderClient
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val v: View = inflater.inflate(R.layout.fragment_maps, container, false)
-        mView = v
-        return mView
+        return inflater.inflate(R.layout.fragment_maps, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         permissionUtils = PermissionUtils(requireActivity(), locationPermission)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
         observeViewModel()
     }
 
-    private fun plotOnMap(map: GoogleMap) {
-        val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
-        val cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 16f)
-        map.animateCamera(cameraUpdate)
-        val marker: View =
-            (requireActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(
-                R.layout.layout_marker_title,
-                null
-            )
-        val bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(ActivityUtils.createDrawableFromView(requireActivity(), marker))
-        for (i in nearbylocationList) {
-            mMap.addMarker(
-                MarkerOptions()
-                    .position(LatLng(i.main.latitude, i.main.longitude))
-                    .icon(bitmapDescriptor)
-            )
-        }
-    }
     @SuppressLint("MissingPermission")
     private fun fetchUserCurrentLocation() {
         if (isPermissionGranted) {
-
-            val fusedLocationProviderClient =
-                LocationServices.getFusedLocationProviderClient(requireActivity())
             fusedLocationProviderClient.lastLocation.addOnSuccessListener(requireActivity()) {
                 if (it != null) {
                     currentLocation = it
@@ -167,8 +139,8 @@ class MapsFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClickListe
         }
     }
 
+    @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
-        fetchUserCurrentLocation()
         googleMap.uiSettings.isMyLocationButtonEnabled = false
         val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
         val currentLocationMarker =
@@ -178,21 +150,21 @@ class MapsFragment : Fragment(), OnMapReadyCallback,GoogleMap.OnMarkerClickListe
                 R.layout.layout_marker_title,
                 null
             )
-        val tvMarkerLocation = marker.findViewById<View>(R.id.txt_marker_title) as TextView
         val bitmapDescriptor = BitmapDescriptorFactory.fromBitmap(
             ActivityUtils.createDrawableFromView(
                 requireActivity(),
                 marker
             )
         )
+
+        imgMyLocation.setOnClickListener{
         for (i in nearbylocationList) {
             googleMap.addMarker(
                 MarkerOptions()
                     .position(LatLng(i.main.latitude, i.main.longitude))
                     .icon(bitmapDescriptor)
             )
-        }
-        tvMarkerLocation.text = requireActivity().getString(R.string.click)
+        }}
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16f))
         googleMap.addMarker(currentLocationMarker)?.showInfoWindow()
